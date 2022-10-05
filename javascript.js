695925b5aaf06f9cab2477f4304c23f3e7958739
Oscar Otero <oskar.oc@gmail.com>
2022-10-05 14:16:03 +0200

    Create operate function and edit index and CSS

---
+++ javascript.js
@@ -0,0 +1,33 @@
+// Math functions
+function add(a, b) {
+    return a + b;
+};
+
+function subtract(a, b) {
+    return a - b;
+};
+
+function multiply(a, b) {
+    return a * b;
+};
+
+function divide(a, b) {
+    return a / b;
+};
+
+function operate(operator, a, b) {
+    switch (operator) {
+        case "+":
+            return add(a, b);
+            break;
+        case "-":
+            return subtract(a, b);
+            break;
+        case "×":
+            return multiply(a, b);
+            break;
+        case "÷":
+            return divide(a, b);
+            break;
+    }
+}
