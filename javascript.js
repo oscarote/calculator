0c9264cead520d0e15d44079192c737fe795a7e9
Oscar Otero <oskar.oc@gmail.com>
2022-10-06 16:23:54 +0200

    Create all JS functions
    All functions needed were created. The calculator
    is almost finished, missing a round decimals
    implementation.

--- javascript.js
+++ javascript.js
@@ -1,33 +1,174 @@
 // Math functions
 function add(a, b) {
-    return a + b;
+    return parseFloat(a) + parseFloat(b);
 };
 
 function subtract(a, b) {
-    return a - b;
+    return parseFloat(b) - parseFloat(a);
 };
 
 function multiply(a, b) {
-    return a * b;
+    return parseFloat(a) * parseFloat(b);
 };
 
 function divide(a, b) {
-    return a / b;
+    return parseFloat(a) / parseFloat(b);
 };
 
 function operate(operator, a, b) {
+    operation += " " + numberOne + " =";
     switch (operator) {
         case "+":
-            return add(a, b);
+            numberOne = add(a, b);
             break;
         case "-":
-            return subtract(a, b);
+            numberOne = subtract(a, b);
             break;
         case "×":
-            return multiply(a, b);
+            numberOne = multiply(a, b);
             break;
         case "÷":
-            return divide(a, b);
+            numberOne = divide(a, b);
             break;
     }
+    refreshScreen();
+    lastResult = numberOne;
 }
+
+// Screen variables
+const upperLine = document.querySelector(".line1");
+const bottomLine = document.querySelector(".line2");
+let numberOne = "";
+let numberTwo = "";
+let operator = "";
+let operation = "";
+let operationFinished = "yes";
+let lastResult = "";
+
+// Event listeners
+const btnClick = document.querySelectorAll("[data-number]");
+btnClick.forEach(btn => btn.addEventListener("click", e => handleNumber(e.target.textContent)));
+
+const equalBtn = document.getElementById("equal");
+equalBtn.addEventListener("click", finishOperation);
+
+const decimalBtn = document.getElementById("decimal");
+decimalBtn.addEventListener("click", () => {
+    numberOne += ".";
+    refreshScreen();
+});
+
+const signChange = document.getElementById("sign");
+signChange.addEventListener("click", () => {
+    numberOne *= -1;
+    refreshScreen();
+});
+
+const operatorBtn = document.querySelectorAll("[data-operator]");
+operatorBtn.forEach(btn => btn.addEventListener("click", e => handleOperator(e.target.textContent)));
+
+// Reset buttons
+
+const resetBtn = document.getElementById("reset");
+resetBtn.addEventListener("click", resetScreen);
+
+const deleteBtn = document.getElementById("delete");
+deleteBtn.addEventListener("click", () => {
+    numberOne = "";
+    refreshScreen();
+});
+
+const backspaceBtn = document.getElementById("backspace");
+backspaceBtn.addEventListener("click", deleteLast);
+
+// Event listener for keyboard
+window.addEventListener('keydown', handleKey);
+
+function handleKey(e) {
+    if (isFinite(e.key)) {
+        handleNumber(e.key);
+    } else if (e.key === "/") {
+        handleOperator("×");
+    } else if (e.key === "*") {
+        handleOperator("÷");
+    } else if (e.key === "+" || e.key === "-") {
+        handleOperator(e.key);
+    } else if (e.key === "=" || e.key === "Enter") {
+        finishOperation();
+    } else if (e.key === "Backspace") {
+        deleteLast();
+    } else if (e.key === "," || e.key === ".") {
+        numberOne += ".";
+        refreshScreen();
+    }
+}
+// Functions for event listeners
+function deleteLast() {
+    let temp = numberOne.toString().slice(0,-1);
+    numberOne = temp;
+    refreshScreen();
+};
+
+function handleNumber(num) {
+    if (operationFinished === "yes") {
+        resetScreen();
+        operationFinished = "no";    
+        numberOne += num;
+        refreshScreen();
+    } else if (operationFinished === "no" && operation.charAt(operation.length - 1) === "=") {
+        numberTwo = lastResult;
+        numberOne += num;
+        operation = numberTwo + " " + operator;
+        refreshScreen();
+    } else {
+        numberOne += num;
+        refreshScreen();
+    }
+};
+
+function handleOperator(ope) {
+    if (!numberTwo && ope === "-") {
+        numberOne = "-";
+        refreshScreen();
+    } else if (operationFinished === "no" && operation.charAt(operation.length - 1) !== "=" && operator !== "") {
+        operate(operator, numberOne, numberTwo);
+        refreshScreen();
+        operator = ope;
+        numberOne = "";
+    }else if (operationFinished === "no" && operation.charAt(operation.length - 1) === "=") {
+        numberTwo = numberOne;
+        operate(operator, numberOne, numberTwo);
+        numberTwo = numberOne;
+        numberOne = "";
+        operator = ope;
+        operation = numberTwo + " " + operator;
+        refreshScreen();
+    } else {
+        numberTwo = numberOne;
+        numberOne = "";
+        operator = ope;
+        operation = numberTwo + " " + operator;
+        refreshScreen();
+    }
+};
+
+function refreshScreen() {
+    bottomLine.textContent = numberOne;
+    upperLine.textContent = operation;
+};
+
+function resetScreen() {
+    numberOne = "";
+    numberTwo = "";
+    operator = "";
+    operation = "";
+    operationFinished = "yes";
+    lastResult = "";
+    refreshScreen();
+};
+
+function finishOperation() {
+    if (numberTwo === "") return;
+    operate(operator, numberOne, numberTwo);
+    operationFinished = "yes";
+};
